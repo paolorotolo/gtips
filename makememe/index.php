a05cc79ec57e0f73562f2e33efc997c4a3464a51
ale1191 <ale1191@hotmail.it>
2015-01-16 21:47:12 +0100

    2 line support add credits image and display it

--- makememe/index.php
+++ makememe/index.php
@@ -1,9 +1,11 @@
 <?php
-	header('Content-type: image/png');
+	//header('Content-type: image/png');
 	$originalImage = "google.png";
-	$text = "Google+ per la tua azienza proviamo\n con un testo molto ma molto lungo";
+	$text = "bene, funziona anche con i testi più lunghi, limitare a 72 caretteri direi";
 	$subtext = "#googleplustips";
+	$marker = "www.alessiomoretto.com";
 	$sizeimgw = "497";
+	$filename = time().".png";
 	$im = imagecreatefrompng($originalImage);
 	if(!$im) {
 		die("im is null");
@@ -13,24 +15,47 @@
 	$size = 20; 
 	$angle = 0;
 	$y = 320;
+	$y2 = 360;
 	$color = imagecolorallocate($im, 255, 255, 255);
-	$fontfile = "Arial.ttf";
+	$fontfile = "Roboto.ttf";
 	$tb = imagettfbbox($size, $angle, $fontfile, $text);
-	$x = ceil(($sizeimgw - $tb[2]) / 2);
-	imagettftext($im,  $size , $angle , $x , $y , $color , $fontfile , $text);
+	if($tb[2]>$sizeimgw-40){//se troppo grande suddivido
+		$textel = wordwrap($text, 39, "<br>");
+		$textel = explode("<br>",$textel);
+		$tb = imagettfbbox($size, $angle, $fontfile, $textel[0]);
+		$x = ceil(($sizeimgw - $tb[2]) / 2);
+		imagettftext($im,  $size , $angle , $x , $y , $color , $fontfile , $textel[0]);
+		$tb = imagettfbbox($size, $angle, $fontfile, $textel[1]);
+		$x = ceil(($sizeimgw - $tb[2]) / 2);
+		imagettftext($im,  $size , $angle , $x , $y2 , $color , $fontfile , $textel[1]);
+	}else{
+		$tb = imagettfbbox($size, $angle, $fontfile, $text);
+		$x = ceil(($sizeimgw - $tb[2]) / 2);
+		imagettftext($im,  $size , $angle , $x , $y , $color , $fontfile , $text);
+	}
 	
 	$size = 20; 
 	$angle = 0;
 	$x = 75; 
 	$y = 420;
 	$color = imagecolorallocate($im, 200, 200, 200);
-	$fontfile = "Arial.ttf";
+	$fontfile = "Roboto.ttf";
 	$tb = imagettfbbox($size, $angle, $fontfile, $subtext);
 	$x = ceil(($sizeimgw - $tb[2]) / 2);
 	imagettftext($im,  $size , $angle , $x , $y , $color , $fontfile , $subtext);
 	
 	
-	$outputImage = "group_icon_1.png";
+	$size = 10; 
+	$angle = 0;
+	$x = 5; 
+	$y = $sizeimgw-10;
+	$color = imagecolorallocate($im, 200, 200, 200);
+	$fontfile = "Roboto.ttf";
+	imagettftext($im,  $size , $angle , $x , $y , $color , $fontfile , $marker);
+	
+	
+	$outputImage = $filename;
 	imagepng($im, $outputImage, 0);
 	imagedestroy($im);
+	print '<img src="'.$filename.'" alt="" />';
 ?>
