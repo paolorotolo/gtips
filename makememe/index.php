7f157bfb221dc2f22d99f0f1095ed6e853c9fa15
ale1191 <ale1191@hotmail.it>
2015-01-17 09:57:30 +0100

    clean code, reorganize makememe folder

--- makememe/index.php
+++ makememe/index.php
@@ -1,11 +1,11 @@
 <?php
 	//header('Content-type: image/png');
-	$originalImage = "google.png";
+	$originalImage = "sourceimg/google.png";
 	$text = "bene, funziona anche con i testi più lunghi, limitare a 72 caretteri direi";
 	$subtext = "#googleplustips";
 	$marker = "www.alessiomoretto.com";
 	$sizeimgw = "497";
-	$filename = time().".png";
+	$filename = "img/".time()."_".rand(0,100).".png";
 	$im = imagecreatefrompng($originalImage);
 	if(!$im) {
 		die("im is null");
@@ -17,7 +17,7 @@
 	$y = 320;
 	$y2 = 360;
 	$color = imagecolorallocate($im, 255, 255, 255);
-	$fontfile = "Roboto.ttf";
+	$fontfile = "font/Roboto.ttf";
 	$tb = imagettfbbox($size, $angle, $fontfile, $text);
 	if($tb[2]>$sizeimgw-40){//se troppo grande suddivido
 		$textel = wordwrap($text, 39, "<br>");
@@ -39,7 +39,7 @@
 	$x = 75; 
 	$y = 420;
 	$color = imagecolorallocate($im, 200, 200, 200);
-	$fontfile = "Roboto.ttf";
+	$fontfile = "font/Roboto.ttf";
 	$tb = imagettfbbox($size, $angle, $fontfile, $subtext);
 	$x = ceil(($sizeimgw - $tb[2]) / 2);
 	imagettftext($im,  $size , $angle , $x , $y , $color , $fontfile , $subtext);
@@ -50,7 +50,7 @@
 	$x = 5; 
 	$y = $sizeimgw-10;
 	$color = imagecolorallocate($im, 200, 200, 200);
-	$fontfile = "Roboto.ttf";
+	$fontfile = "font/Roboto.ttf";
 	imagettftext($im,  $size , $angle , $x , $y , $color , $fontfile , $marker);
 	
 	
