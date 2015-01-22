a52374e1619e106ea3cb9a3db57a843af5c6951b
Paolo Rotolo <paolorotolo@ubuntu.com>
2015-01-22 20:33:33 +0100

    Options now disappears if not nedded.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
-$("#style").bind("onload",function () {
-alert("Hello world!");
-});
+    
+    // Check the value of Style's dropdown when it changes;
+	$('.eventschange').bind("DOMSubtreeModified",function(){
+        checkDropDown();
+    });
+    
+    function checkDropDown(){
+        var style = $('#style').find(":selected").text();
+        
+        // If style is Small, hide other options;
+        if (style === "Small"){
+            $("#style2").hide();
+        
+        // If not, show them;
+        } else if (style === "Large") {
+            $("#style2").show();
+            $("#background").hide();
+            
+        } else {
+            $("#style2").show();
+            $("#background").show();
+        };
+    };
 });
